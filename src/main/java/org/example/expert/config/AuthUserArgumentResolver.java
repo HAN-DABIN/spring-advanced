bc2748d304f5e18cc188a1aa860613f8ed3f2b81
HAN-DABON <sinmint11@gmail.com>
2026-05-07 17:21:49 +0900

    레벨1-2: AuthUserArgumentResolver 수동 등록

--- src/main/java/org/example/expert/config/AuthUserArgumentResolver.java
+++ src/main/java/org/example/expert/config/AuthUserArgumentResolver.java
@@ -14,6 +14,7 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
 @Component
+
 public class AuthUserArgumentResolver implements HandlerMethodArgumentResolver {
 
     @Override
